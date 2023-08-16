123db57e95f271c78721bc35a9a4696971e9ef92
TheGuestFromBeyond <theguestfrombeyond@protonmail.com>
2023-08-16 22:19:27 +0300

    Implement a language detection service

--- src/main/java/com/beyond/languagedetectorservice/service/LanguageDetectorService.java
+++ src/main/java/com/beyond/languagedetectorservice/service/LanguageDetectorService.java
@@ -1,10 +1,40 @@
 package com.beyond.languagedetectorservice.service;
 
+import com.google.common.base.Optional;
+import com.optimaize.langdetect.LanguageDetector;
+import com.optimaize.langdetect.LanguageDetectorBuilder;
+import com.optimaize.langdetect.i18n.LdLocale;
+import com.optimaize.langdetect.ngram.NgramExtractors;
+import com.optimaize.langdetect.profiles.LanguageProfile;
+import com.optimaize.langdetect.profiles.LanguageProfileReader;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class LanguageDetectorService {
     public String detectLanguageOf(String text) {
-        return text;
+        if (!StringUtils.hasText(text)) {
+            return "";
+        }
+        try {
+            List<LanguageProfile> languageProfiles = new LanguageProfileReader().readAllBuiltIn();
+
+            LanguageDetector languageDetector = LanguageDetectorBuilder.create(NgramExtractors.standard())
+                    .withProfiles(languageProfiles)
+                    .build();
+
+            Optional<LdLocale> detected = languageDetector.detect(text);
+            if (detected.isPresent()) {
+                return detected.get().getLanguage();
+            } else {
+                return "Language cannot be detected";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
